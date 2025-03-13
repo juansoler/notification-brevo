6fba012a00bcd383e5e6fa99aef579f17b6bdd2f
kb-0912 <78142541+kb-0912@users.noreply.github.com>
2025-03-13 17:14:47 +0700

    fix abandone cart

--- src/workflows/send-abandoned-cart.ts
+++ src/workflows/send-abandoned-cart.ts
@@ -10,7 +10,7 @@ type WorkflowInput = {
 export const sendAbandonedCartWorkflow = createWorkflow(
   "send-abandoned-cart",
   ({ cartId }: WorkflowInput) => {
-    const cartStep = useQueryGraphStep({
+    const { data: carts } = useQueryGraphStep({
       entity: "cart",
       fields: [
         "id",
@@ -25,26 +25,19 @@ export const sendAbandonedCartWorkflow = createWorkflow(
       filters: { id: cartId },
     });
 
-    const prepareNotificationStep = createStep(
-      "prepare-abandoned-cart-notification",
-      async (input: { data: any[] }) => {
-        const carts = input.data;
+   
 
-        const notificationData: CreateNotificationDTO[] = [
+    const notificationData: CreateNotificationDTO[] = [
           {
             to: carts[0].email,
             channel: "email",
             template: "cart.abandoned",
             data: { cart: carts[0] },
           },
-        ];
+    ];
 
 
-        return new StepResponse(notificationData);
-      }
-    );
-
-    const notificationData = prepareNotificationStep(cartStep);
+        
     const notificationStep = sendNotificationStep(notificationData);
 
     return new WorkflowResponse(notificationStep);
